5454eeaa7c764dc1f0946a66e325632374cce2ac
ZaraQureshi <qzara819@gmail.com>
2025-11-27 12:23:46 +0530

    add zod validationb

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import { db } from "./server";
 import { users } from "./db/schema";
+import z, { int, number, string } from "zod";
+
+
+const userSchema=z.object({
+    name:string().min(5),
+    email:string().email(),
+    age:number().min(5)
+})
 
 const router = Router();
 router.get('/users',async(req,res)=>{
     const result=await db.select().from(users);
     return res.json(result);
+})
+
+router.post('/users',async(req,res)=>{
+    const {name,email,age}=await userSchema.parse(req.body);
+    console.log(name)
+    const result=await db.insert(users).values({name,email,age}).returning();
+    return res.json(result)
 })
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,24 @@ import cors from "cors";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import routes from "./routes";
+import fs from "fs";
 
-const app=Express();
+const app = Express();
 
 app.use(cors());
 app.use(Express.json());
 
 // connect sqlite
-const sqlite = new Database("./db/database.db");
-export const db=drizzle(sqlite)
+// try {
+// Ensure db folder exists
+if (!fs.existsSync("./db")) {
+  fs.mkdirSync("./db");
+}
+  const sqlite = new Database("./db/database.db");
+  console.log(sqlite)
+  export const db = drizzle(sqlite)
+// } catch (e) { console.error(e) };
+
 
 // API routes
 app.use("/api", routes);
