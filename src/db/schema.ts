cbbec8deb1f96eb1532f4b4cd31ceeb846e0eacd
ZaraQureshi <qzara819@gmail.com>
2025-11-27 12:23:46 +0530

    add zod validationb

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,9 @@
 import { text } from "drizzle-orm/sqlite-core";
 import { integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const users=sqliteTable('users',{
-    id:integer('id').primaryKey({autoIncrement:true}),
-    name:text('name')
+export const users = sqliteTable('users', {
+    id: integer('id').primaryKey({ autoIncrement: true }),
+    name: text('name').notNull(),
+    age: integer().notNull(),
+    email: text().notNull().unique(),
 })
